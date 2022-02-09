36a1b4809629d927caa6a2e951ec535e41b6a5cc
Sachin Ambegave <sachinbalajiambegave@gmail.com>
2022-02-10 03:04:56 +0530

    Initial Commit - Project Structure

---
+++ r-Swift/r-Swift/App/rSwift/ViewController/RSwiftRedditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RSwiftRedditViewController.swift
+//  r-Swift
+//
+//  Created by Sachin Ambegave on 10/02/22.
+//
+
+import UIKit
+
+class RSwiftRedditViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Swift News"
+        view.backgroundColor = .green
+    }
+}
---
+++ r-Swift/r-Swift/Application Support/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  r-Swift
+//
+//  Created by Sachin Ambegave on 10/02/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
